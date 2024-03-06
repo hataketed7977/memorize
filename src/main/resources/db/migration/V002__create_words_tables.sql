4643e16f9007f3873de47a5b1caad8b3d8f9ef0c
Zhang Gang <gzhang@thoughtworks.com>
2024-03-06 14:52:40 +0800

    add indexes

--- src/main/resources/db/migration/V002__create_words_tables.sql
+++ src/main/resources/db/migration/V002__create_words_tables.sql
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS t_unfamiliar_words
 (
     id         VARCHAR(255) NOT NULL PRIMARY KEY,
-    dic_id     VARCHAR(255) NOT NULL,
+    dic_id     VARCHAR(255) NOT NULL UNIQUE,
     created_at TIMESTAMP(6) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS t_incorrect_words
 (
     id         VARCHAR(255) NOT NULL PRIMARY KEY,
-    dic_id     VARCHAR(255) NOT NULL,
+    dic_id     VARCHAR(255) NOT NULL UNIQUE,
     count      INT          NOT NULL DEFAULT 1,
     created_at TIMESTAMP(6) NOT NULL,
     updated_at TIMESTAMP(6) NOT NULL
@@ -17,6 +17,6 @@ CREATE TABLE IF NOT EXISTS t_incorrect_words
 CREATE TABLE IF NOT EXISTS t_familiar_words
 (
     id         VARCHAR(255) NOT NULL PRIMARY KEY,
-    dic_id     VARCHAR(255) NOT NULL,
+    dic_id     VARCHAR(255) NOT NULL UNIQUE,
     created_at TIMESTAMP(6) NOT NULL
 );
---
+++ src/main/resources/db/migration/V004__add_indexes.sql
@@ -0,0 +1,5 @@
+CREATE INDEX t_vocabularies_study_index ON t_vocabularies (study);
+
+CREATE INDEX t_vocabularies_created_at_index ON t_vocabularies (created_at);
+
+CREATE INDEX t_vocabularies_created_at_desc_index ON t_vocabularies (created_at DESC);
