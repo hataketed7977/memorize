6b5f57412d54c5141b02ed3d0f36fa25e8c4be89
Zhang Gang <gzhang@thoughtworks.com>
2024-03-12 16:35:42 +0800

    add open browser

--- src/main/kotlin/com/mavi/memorize/MemorizeApplication.kt
+++ src/main/kotlin/com/mavi/memorize/MemorizeApplication.kt
@@ -1,12 +1,31 @@
 package com.mavi.memorize
 
-import com.vaadin.flow.theme.Theme
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.ConfigurableApplicationContext
+import java.awt.Desktop
+import java.net.URI
+
 
 @SpringBootApplication
 class MemorizeApplication
 
 fun main(args: Array<String>) {
-    runApplication<MemorizeApplication>(*args)
+    val app = runApplication<MemorizeApplication>(*args)
+    openHomePage(app)
+
+}
+
+fun openHomePage(app: ConfigurableApplicationContext) {
+    val open = app.environment.getProperty("browser.open").toBoolean()
+    if (!open) return
+    try {
+        val host = app.environment.getProperty("browser.host").toString()
+        val port = app.environment.getProperty("browser.port").toString()
+        val path = app.environment.getProperty("browser.path").toString()
+        System.setProperty("java.awt.headless", "false")
+        Desktop.getDesktop().browse(URI("$host:$port$path"))
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
 }
