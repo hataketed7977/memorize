c8fe3094dc75598f77efd6bf221e38f2c6b5368e
Zhang Gang <gzhang@thoughtworks.com>
2024-03-12 15:28:39 +0800

    fix test can not run multi issue

--- src/test/kotlin/com/mavi/memorize/DBTest.kt
+++ src/test/kotlin/com/mavi/memorize/DBTest.kt
@@ -3,22 +3,23 @@ package com.mavi.memorize
 import org.junit.jupiter.api.extension.*
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.testcontainers.containers.PostgreSQLContainer
 
 
 @SpringBootTest
 @ExtendWith(DatabaseContainerExtension::class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 annotation class DBTest
 
 class DatabaseContainerExtension : BeforeAllCallback, AfterAllCallback, AfterEachCallback {
-    private val postgresContainer = PostgreSQLContainer("postgres:16-alpine").withReuse(true)
+    private val postgresContainer = PostgreSQLContainer("postgres:16-alpine")
+        .withReuse(true)
+
     override fun beforeAll(context: ExtensionContext?) {
         postgresContainer.start()
-        System.setProperty(
-            "spring.datasource.url",
-            "jdbc:postgresql://${postgresContainer.host}:${postgresContainer.getMappedPort(5432)}/${postgresContainer.databaseName}"
-        )
+        System.setProperty("spring.datasource.url", postgresContainer.jdbcUrl)
         System.setProperty("spring.datasource.username", postgresContainer.username)
         System.setProperty("spring.datasource.password", postgresContainer.password)
     }
