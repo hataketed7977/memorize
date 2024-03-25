5532fd2c531e16c8ad7b6934cec526034c206a2c
Zhang Gang <gzhang@thoughtworks.com>
2024-03-25 16:41:32 +0800

    update data

--- data/t_unfamiliar_words.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO public.t_unfamiliar_words (id, vocabulary_id, created_at) VALUES ('4c3aa03e-abb2-4c5f-afdf-27fd68932c01', 'f9e4e1b4-5e80-4075-a4d9-63e4013af233', '2024-03-14 07:03:22.099362');
